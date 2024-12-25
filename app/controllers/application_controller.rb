d2d82fb50e0be60eff4c70c8fb1860148e9ad325
Pavel Gorohovsky <pavel-gorohovsky@yandex.com>
2024-12-26 00:19:46 +0200

    Added ActiveRecord::RecordNotFound interception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::API
   include ParamsGuard
+
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_response
+
+  private
+
+  def record_not_found_response(error)
+    render json: { error: error.message }, status: 404
+  end
 end
